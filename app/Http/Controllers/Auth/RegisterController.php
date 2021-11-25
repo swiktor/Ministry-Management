020be42afdec4f398107a5468862966b527c4bac
Szymon Wiktor <szymonwiktor+informatyk@gmail.com>
2021-11-25 15:16:43 +0100

    Split name and surname in register form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,6 +55,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'congregation' => ['required', 'integer'],
@@ -71,6 +72,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'surname' => $data['surname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'goal_id' => '1',
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,6 +28,22 @@
                             </div>
 
                             <div class="form-group row">
+                                <label for="surname"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('main.Surname') }}</label>
+                                <div class="col-md-6">
+                                    <input id="surname" type="text"
+                                        class="form-control @error('surname') is-invalid @enderror" name="surname"
+                                        value="{{ old('surname') }}" required autocomplete="surname" autofocus>
+
+                                    @error('surname')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
                                 <label for="email"
                                     class="col-md-4 col-form-label text-md-right">{{ __('main.E-Mail Address') }}</label>
 
